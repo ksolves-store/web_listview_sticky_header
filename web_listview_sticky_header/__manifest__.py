12a4c845c854d8154edb05378276372063be47a5
akanshksolves <akansh.gautam@ksolves.com>
2019-11-07 12:08:23 +0530

    banner added

--- web_listview_sticky_header/__manifest__.py
+++ web_listview_sticky_header/__manifest__.py
@@ -21,7 +21,7 @@
     'license': 'LGPL-3',
     'category': 'Tools',
     'version': '13.0.1.0.0',
-    'images': ['static/description/main.jpg'],
+    'images': ['static/description/main.png'],
     'depends': ['base'],
 
     'data': [
