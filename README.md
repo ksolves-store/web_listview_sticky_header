9fbdb05de20a0e04e06155e342e9a0ac61b64bb6
KsolvesStore <store@ksolves.com>
2020-12-17 15:04:21 +0530

    inital commit for v14

---
+++ README.md
@@ -0,0 +1,17 @@
+# Web List View Sticky Header
+
+This application allows users to have a better user experience.
+
+## Features
+
+Enhance the user experience by sticking the header of Tree View so that user will never get lost in a long list of records.
+
+### Installation
+
+-   No external library is used.
+-   For odoo v12.
+
+## Authors
+
+-   Developed and maintained by - [Ksolves India Pvt. Ltd.](https://www.ksolves.com/)
+
---
+++ web_listview_sticky_header/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "List View Sticky Header",
+
+    'summary': """
+      List View Sticky Header v14.0""",
+
+    'description': """
+	- Sticky Header
+	- List Sticky Header
+	- Header Sticky
+	- Fix Header
+	- Header Fix
+	- Table Header Fix
+	- Table List Fix 
+	- Table Hedar Sticky
+	Tree view header, 
+	header freeze, 
+	odoo custom view app, 
+	sticky view in odoo, 
+	odoo view app, 
+	listviewstickyheader, 
+	all in one sticky, 
+	freeze view in odoo, 
+	freeze list view , 
+	sticky pivot view, 
+	sticky pivot view header, 
+	fixed pivot view header, 
+	freeze header, 
+	pivot view freeze header,
+    """,
+
+    'author': "Ksolves India Pvt. Ltd.",
+    'website': "https://www.ksolves.com/",
+    'license': 'LGPL-3',
+    'category': 'Tools',
+    'version': '14.0.1.0.0',
+    'images': ['static/description/main.jpg'],
+    'depends': ['base'],
+
+    'data': [
+        'views/assets.xml',
+    ],
+
+}
