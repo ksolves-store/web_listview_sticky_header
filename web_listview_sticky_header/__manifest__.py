094d2a5249306456571a8da031f3d73867096167
Vinisha Soni <vinisha.soni@ksolves.com>
2023-04-10 11:42:09 +0530

    Changed banner from v15 to v16.

--- web_listview_sticky_header/__manifest__.py
+++ web_listview_sticky_header/__manifest__.py
@@ -36,7 +36,7 @@
     'currency': 'EUR',
     'category': 'Tools',
     'version': '16.0.1.0.0',
-    'images': ['static/description/List_View_Stick_Header.jpg'],
+    'images': ['static/description/List_View_Sticky_Header.jpg'],
     'depends': ['base'],
 
     'assets': {
